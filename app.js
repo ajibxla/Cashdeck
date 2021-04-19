7e59d1c6c6b12dce394ddf7ae4c546f18d43a76c
ajibxla <82827386+ajibxla@users.noreply.github.com>
2021-04-19 16:14:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+
+
+//tl.to(".heading", { y: "0%", duration: 1, stagger: 0.25 });
+
+//tl.to(".landing-img", { y: "-100%", duration: 1.5, delay: 0.5 });
+
+tl.fromTo(".heading", { opacity: -1 }, { opacity: 1, duration: 5,});
+
+gsap.from(".landing-img", {duration: 5, x: 300, opacity: 0, scale: 0.5, delay: 2});
+
